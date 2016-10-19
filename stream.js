b8b8ec43ab2fc18082c6cffd22aed1c6a9d834ef
Sam <orgillsams@gmail.com>
2016-10-19 21:05:32 +0100

    edit

--- stream.js
+++ stream.js
@@ -21,7 +21,19 @@ var fs = require('fs');
 var http = require('http');
 
 http.createServer(function(request, response){
-	var newFile = fs.createWriteStream("readme_copy.md");
+	var newFile = fs.createWriteStream("progress_copy.md");
+	var fileBytes = request.headers['content-length'];
+	var uploadedBytes = 0;
+
+	request.on('readable', function(){
+		var chunk = null;
+		while(null !== (chunk = request.read())){
+			uploadedBytes += chunk.length;
+			var progress = (uploadedBytes / fileBytes)*100;
+			response.write("progress: " + parseInt(progress, 10) + "%\n");
+		}
+	});
+	
 	request.pipe(newFile);
 
 	request.on('end', function(){
